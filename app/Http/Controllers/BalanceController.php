6bd5c1cb4ddf549bcc9b7ed7771075384d713aa1
RandyShin <randy@ziotes.com>
2017-11-07 12:52:48 +0900

    compelete prices 2

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -16,12 +16,12 @@ class BalanceController extends Controller
 
         $ext = [2200,2201,2202,2203,2204,2205,2206,2207,2208,2209,2210,2211,2212,2213,2214,2215,2216,2217,2218,2219,2220];
 
-        $cdrs = Cdr::whereIn('src', $ext)->orderBy('calldate', 'desc')
-            ->paginate(15);
 
-        $prices = Cdr::whereIn('src', $ext)->get();
+        $cdrs = Cdr::whereIn('src', $ext)->whereRaw('LENGTH(dst) != 4')->orderBy('calldate', 'desc')->paginate(15);
 
-        $cnt = Cdr::whereIn('src', $ext)-> count();
+        $prices = Cdr::whereIn('src', $ext)->whereRaw('LENGTH(dst) != 4')->get();
+
+        $cnt = Cdr::whereIn('src', $ext)->whereRaw('LENGTH(dst) != 4')->count();
 
         $total = 0;
 
@@ -30,6 +30,15 @@ class BalanceController extends Controller
             if(substr($price->dst,'0','2') === '01');
             {
                 $total = $total + ceil($price->billsec/10)*6.8;
+            }
+            if(substr($price->dst, 0,2)==='86'){
+                $total = $total + ceil($price->billsec/60)*22;
+            }
+            if((substr($price->dst, 0,3)==='070') || (substr($price->dst,0,3) === '050' )) {
+                $total = $total + ceil($price->billsec/180)*45;
+            }
+            if((substr($price->dst, 0,2)!='01') && (substr($price->dst,0,2) != '86' ) && (substr($price->dst,0,3) != '070' ) && (substr($price->dst,0,3) != '050' )) {
+                $total = $total + ceil($price->billsec/180)*32;
             }
         }
 
